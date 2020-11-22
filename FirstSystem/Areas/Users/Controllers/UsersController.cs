74ff559897ca3100a3c4dcfb18e86a8a760e209a
LucianoDiaz <luchodiaz1995@gmail.com>
2020-11-22 15:39:34 -0300

    Primer Commit

---
+++ FirstSystem/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstSystem.Areas.Users.Controllers
+{
+    [Area("Users")]
+    public class UsersController : Controller
+    {
+        /// <summary>
+        /// Este es el metodo de accion que se va a ejecutar
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
